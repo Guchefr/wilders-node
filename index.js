03026cd696e3d4b28c8175dd8ebd5ec209fc938c
Gustavo Cardoso <gustavo.che@live.fr>
2024-10-15 11:21:45 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import chalk from "chalk";
+
+console.log(
+    chalk.blue("Lassana")
+);
+
+console.log(
+    chalk.red("Karim")
+);
+
+console.log(
+    chalk.magenta("Juju")
+);
+
+console.log(
+    chalk.greenBright("Kler")
+);
+
